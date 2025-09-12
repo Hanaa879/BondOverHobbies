72fc2419683107d90ad5312f8cd2dc2cc9ce549c
Hanaa Yesenia.H <hanaayesenia.h1.8253@gmail.com>
2025-09-12 15:34:27 +0000

    make logo like this

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -11,42 +11,23 @@ export function Logo({ className, ...props }: React.ComponentProps<"svg">) {
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className={cn("h-6 w-6 text-primary", className)}
+      className={cn("h-6 w-6", className)}
       {...props}
     >
-      <title>BondOverHobbies Logo</title>
-      <g transform="translate(12, 12)">
-        {/* Pin 1 */}
-        <g transform="rotate(0) translate(0, -3.5)">
-          <path d="M0,3.5 L0,1.5" />
-          <path d="M-0.5, -2 A 1 1 0 0 1 0.5 -2" />
-          <path d="M0.5,-2 L1.5,-2 A 1 1 0 0 1 1.5, -4 L-1.5, -4 A 1 1 0 0 1 -1.5, -2 L-0.5,-2" />
-        </g>
-        {/* Pin 2 */}
-        <g transform="rotate(72) translate(0, -3.5)">
-          <path d="M0,3.5 L0,1.5" />
-          <path d="M-0.5, -2 A 1 1 0 0 1 0.5 -2" />
-          <path d="M0.5,-2 L1.5,-2 A 1 1 0 0 1 1.5, -4 L-1.5, -4 A 1 1 0 0 1 -1.5, -2 L-0.5,-2" />
-        </g>
-        {/* Pin 3 */}
-        <g transform="rotate(144) translate(0, -3.5)">
-          <path d="M0,3.5 L0,1.5" />
-          <path d="M-0.5, -2 A 1 1 0 0 1 0.5 -2" />
-          <path d="M0.5,-2 L1.5,-2 A 1 1 0 0 1 1.5, -4 L-1.5, -4 A 1 1 0 0 1 -1.5, -2 L-0.5,-2" />
-        </g>
-        {/* Pin 4 */}
-        <g transform="rotate(216) translate(0, -3.5)">
-          <path d="M0,3.5 L0,1.5" />
-          <path d="M-0.5, -2 A 1 1 0 0 1 0.5 -2" />
-          <path d="M0.5,-2 L1.5,-2 A 1 1 0 0 1 1.5, -4 L-1.5, -4 A 1 1 0 0 1 -1.5, -2 L-0.5,-2" />
-        </g>
-        {/* Pin 5 */}
-        <g transform="rotate(288) translate(0, -3.5)">
-          <path d="M0,3.5 L0,1.5" />
-          <path d="M-0.5, -2 A 1 1 0 0 1 0.5 -2" />
-          <path d="M0.5,-2 L1.5,-2 A 1 1 0 0 1 1.5, -4 L-1.5, -4 A 1 1 0 0 1 -1.5, -2 L-0.5,-2" />
-        </g>
-      </g>
+        <title>BondOverHobbies Logo</title>
+        <path
+            d="M12 20.5C12 22 10.8417 22 10 22C8.15833 22 7 21 7 19C7 17 10 16 12 14"
+            stroke="currentColor"
+        />
+        <path
+            d="M12 14C14 12 17 10 17 6C17 2 15 2 12 2C9 2 7 2 7 6"
+            stroke="currentColor"
+        />
+        <path
+            d="M10.382,14.7459L12,11.5l1.618,3.2459,3.579.52-2.59,2.524.611,3.5642L12,20.5l-3.218,1.6942.611-3.5642-2.59-2.524,3.579-.52Z"
+            stroke="currentColor"
+            fill="hsl(var(--primary))"
+        />
     </svg>
   );
 }
