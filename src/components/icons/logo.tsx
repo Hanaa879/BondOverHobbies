cddb3ae10f8ae99cf26c2942d913375b71ee317c
Hanaa Yesenia.H <hanaayesenia.h1.8253@gmail.com>
2025-09-12 15:29:33 +0000

    a star connects to it

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -18,7 +18,9 @@ export function Logo({ className, ...props }: React.ComponentProps<"svg">) {
       <path d="M19.33 6.33a2.5 2.5 0 0 0-3.66-3.66l-12 12a2.5 2.5 0 0 0 3.66 3.66l12-12z" />
       <path d="M13.5 1.5l-4 4" />
       <path d="M19.5 7.5l-4 4" />
-      <path d="M4.67 17.67a2.5 2.5 0 1 0-3.34-3.34" />
+      <path d="m4.63 14.2-.07-.07a2.5 2.5 0 1 0-3.46 3.53" />
+      <path d="m2.1 18.9 1.4-1.4" />
+      <path d="m1.4 17.5 1.4 1.4" />
     </svg>
   );
 }
